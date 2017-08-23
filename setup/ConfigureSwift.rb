97b03c51a72a635445c2b49d4cba613cc0b150c7
jcarroll-mediafly <jcarroll@mediafly.com>
2017-08-23 08:58:25 -0500

    Update ConfigureSwift.rb

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -12,33 +12,9 @@ module Pod
     end
 
     def perform
-      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
+      keep_demo = true
 
-      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Quick", "None"]).to_sym
-      case framework
-        when :quick
-          configurator.add_pod_to_podfile "Quick', '~> 1.0.0"
-          configurator.add_pod_to_podfile "Nimble', '~> 5.1.1"
-          configurator.set_test_framework "quick", "swift"
-
-        when :none
-          configurator.set_test_framework "xctest", "swift"
-      end
-
-      snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
-      case snapshots
-        when :yes
-          configurator.add_pod_to_podfile "FBSnapshotTestCase"
-          
-          if keep_demo == :no
-              puts " Putting demo application back in, you cannot do view tests without a host application."
-              keep_demo = :yes
-          end
-
-          if framework == :quick
-              configurator.add_pod_to_podfile "Nimble-Snapshots"
-          end
-      end
+      configurator.set_test_framework "xctest", "swift"
 
       Pod::ProjectManipulator.new({
         :configurator => @configurator,
